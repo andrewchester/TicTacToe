ba3c0957018ff06e3a74d459ea329f076730fb0a
andrewchester <476724@bsd48.org>
2018-09-08 21:10:23 -0700

    Removed board class, just going to use main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,63 @@
 /*
  * Andrew Chester
  * TicTacToe
- * value, 0 = empty, 1 = x, 2 = 0
+ * value: 0 = empty, 1 = X, 2 = O
  */
 #include <iostream>
 
-#include "board.h"
+struct Cell
+{
+  int x, y, value;
+};
 
-using namespace std;
+void printBoard(Cell* board[3][3])
+{
+  std::cout << "  1 2 3 " << std::endl;
+  for(int x = 0; x < 3; x++)
+  {
+    std::cout << (97 + x);
+    for(int y = 0; y < 3; y++)
+    {
+      if((*board[x][y]).value == 0)
+      {
+        std::cout << " ";
+      }
+      else if((*board[x][y]).value == 1)
+      {
+        std::cout << "X";
+      }
+      else if((*board[x][y]).value  == 2)
+      {
+        std::cout << "O";
+      }
+    }
+    std::cout << std::endl;
+  }
+}
 
 int main()
 {
+  Cell board[3][3];
+  Cell* boardP[3][3];
+  
+  for(int x = 0; x < 3; x++)
+  {
+    for(int y = 0; y < 3; y++)
+    {
+      board[x][y].x = x;
+      board[x][y].y = y;
+      board[x][y].value = 0;
+      boardP[x][y] = &board[x][y];
+    }
+  }
+
   //Variables and pointers for storing whose turn it is and whether they're playing
   bool playerOneTurn = true;
   bool* turnP = &playerOneTurn;
   bool playing = true;
   bool* playingP = &playing;
 
-  Board board;
-
-  board.iniBoard();
-  board.printBoard();
-  board.printValues();
+  printBoard(boardP);
 
   return 0;
 }
