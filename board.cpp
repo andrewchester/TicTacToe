009378d179462f2de64509c8e4ae55c576f90b9e
andrewchester <476724@bsd48.org>
2018-09-10 23:04:38 -0700

    Added comments, there is no condition for if its a tie, need to implement

--- board.cpp
+++ board.cpp
@@ -1,25 +1,27 @@
+//Board class, handles checking win, updating board, board initialization, and also printing the board out
 #include <iostream>
 
 #include "board.h"
 
+//Cell struct for holding the values and positions of values
 struct Board::Cell{
   	int x, y, value;
 };	
-
+//Constructor, initializes the board
 Board::Board()
 {
 	for(int x = 0; x < 3; x++)
 	{
 		for(int y = 0; y < 3; y++)
 		{
-			board[x][y] = new Cell();
+			board[x][y] = new Cell(); //Use new so the cells are allocated to the heap and not overwritten at the end of the function
 			(*board[x][y]).x = x;
 			(*board[x][y]).y = y;
-			(*board[x][y]).value = 0;
+			(*board[x][y]).value = 0; //Start all cells at 0
 		}
 	}
 }
-
+//A function that changes the value depending on whose turn it was
 void Board::updateBoard(int x, int y, bool* turn)
 {
 	if(*turn)
@@ -27,25 +29,30 @@ void Board::updateBoard(int x, int y, bool* turn)
 	else
 		(*board[x][y]).value = 2;
 }
+//Returns true if the space isn't empty
 bool Board::spaceOccupied(int x, int y)
 {
 	if((*board[x][y]).value != 0)
 		return true;
 	return false;
 }
+//A function for testing if there are 3 in a row in the columns
 bool Board::testColumns()
 {
+	//Total count of x's and o's in a column
 	int xCount = 0;
 	int oCount = 0;
 
 	for(int row = 0; row < 3; row++)
 	{
+		//If either of them are three at the end of the loop, return true early
 		if(xCount == 3 || oCount == 3)
 			return true;
-		xCount = 0;
+		xCount = 0; //Reset the values
 		oCount = 0;
 		for(int column = 0; column < 3; column++)
 		{
+			//Increment the values according to the value of the cell
 			if((*board[row][column]).value == 1)
 			{
 				xCount++;
@@ -58,19 +65,23 @@ bool Board::testColumns()
 	}
 	return false;
 }
+//Function for testing rows
 bool Board::testRows()
 {
+	//Total count of x's and o's in a row
 	int xCount = 0;
 	int oCount = 0;
 
 	for(int column = 0; column < 3; column++)
 	{
+		//Return true early if we find a row with 3 in a row
 		if(xCount == 3 || oCount == 3)
 			return true;
-		xCount = 0;
+		xCount = 0; //Reset values
 		oCount = 0;
 		for(int row = 0; row < 3; row++)
 		{
+			//Incremement values accordingly
 			if((*board[row][column]).value == 1)
 			{
 				xCount++;
@@ -83,11 +94,13 @@ bool Board::testRows()
 	}
 	return false;
 }
+//Tests the diagonals
 bool Board::testDiags()
 {
-	int xCount = 0;
+	int xCount = 0; //Total count of x's and o's
 	int oCount = 0;
 
+	//Loops from top left to bottom right and increments the values accordingly
 	for(int i = 0; i < 3; i++)
 	{
 		if((*board[i][i]).value == 1)
@@ -95,12 +108,14 @@ bool Board::testDiags()
 		else if((*board[i][i]).value == 2)
 			oCount++;
 	}
+	//If there are 3 in a row, return true
 	if(xCount == 3 || oCount == 3)
 		return true;
 
-	xCount = 0;
+	xCount = 0; //Reset the counter variables
 	oCount = 0;
 
+	//The next three if/else if statements are for testing each cell from top right to bottom left individually and incrementing the appropriate variables
 	if((*board[2][0]).value == 1)
 		xCount++;
 	else if((*board[2][0]).value == 2)
@@ -116,14 +131,16 @@ bool Board::testDiags()
 	else if((*board[0][2]).value == 2)
 		oCount++;
 
+	//If there are there in a row of either x's or o's, return true
 	if(xCount == 3 || oCount == 3)
 		return true;
 
 	return false;
 }
-
+//A function for testing if there is a 3 in a row on the board
 bool Board::testWin(bool* won, bool* turn)
 {
+	//If there is 1 three in a row on the board, return true and set the won variable depending on whoever moved last
 	if(testColumns() || testRows() || testDiags())
 	{
 		if(*turn)
@@ -132,19 +149,20 @@ bool Board::testWin(bool* won, bool* turn)
 			*won = false;
 		return true;
 	}
+	return false;
 }
-
+//A function that clears the board
 void Board::clear()
 {
 	for(int x = 0; x < 3; x++)
 	{
 		for(int y = 0; y < 3; y++)
 		{
-			(*board[x][y]).value = 0;
+			(*board[x][y]).value = 0; //Loop through all the values and set them to empty
 		}
 	}
 }
-
+//A function that tests if the board is full
 bool Board::isFull()
 {
 	int count = 0;
@@ -152,23 +170,25 @@ bool Board::isFull()
 	{
 		for(int y = 0; y < 3; y++)
 		{
+			//Counting up the variable if it isn't empty
 			if((*board[x][y]).value != 0)
 				count++;
 		}
 	}
-	if(count == 9)
+	if(count == 9) //Returning true if all of them are filled
 		return true;
 	return false;
 }
-
+//Function for printing the board
 void Board::printBoard()
 {
-	std::cout << "  1 2 3 "<< std::endl;
+	std::cout << "  1 2 3 "<< std::endl; //The 1 2 3 at the top
     for(int x = 0; x < 3; x++)
   	{
-    	std::cout << (char)(97 + x) << " ";
+    	std::cout << (char)(97 + x) << " "; //Prints a, b, or c depending on the ascii character value
     	for(int y = 0; y < 3; y++)
     	{
+    		//Depending on  the value of the cell, print a certain character
      		if((*board[x][y]).value == 2)
 		    {
 		    	std::cout << "O";
@@ -181,12 +201,12 @@ void Board::printBoard()
 		    {
 		    	std::cout << " ";
 		    }
-		   	std::cout << " ";
+		   	std::cout << " "; //Space the chacters out in the row
     	}
-     	std::cout << std::endl;
+     	std::cout << std::endl; //Return after the end of each row
   	}
 }
-
+//Prints out the values of the pointer array, used for bugtesting
 void Board::printValues()
 {
 	for(int x = 0; x < 3; x++)
--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/*
+* Andrew Chester - TicTacToe
+* A text based tictactoe program with 2 players
+*/
 #include <iostream>
 
 #include "board.h"
@@ -6,25 +10,26 @@ using namespace std;
 
 int main()
 {
-  bool playerOneTurn = true;
-  bool* turnP = &playerOneTurn;
-  bool playing = true;
+  bool playerOneTurn = true; //Keep tracks of turns
+  bool* turnP = &playerOneTurn; 
+  bool playing = true; //Boolean for the while loop(although it just uses break anyways)
   bool* playingP = &playing;
-  bool playerWon = true;
+  bool playerWon = true; //Boolean for which player won
   bool* playerWonP = &playerWon;
 
-  char choice[3];
-  char playAgain;
-  int x, y;
+  char choice[3]; //The choice of the player(a1, a2, etc)
+  char playAgain; //If they want to play again
+  int x, y; //The x,y of their choice
+  int xWins = 0, oWins = 0; //Number of wins by each player
 
-  Board board;
+  Board board; //Game board object
 
-  GAME_START:
+  GAME_START:  //Return here is they want to play again
   while(playing)
   {
-  	board.printBoard();
+  	board.printBoard(); //Print out the board at the start of each turn
 
-  	GET_INPUT:
+  	GET_INPUT: //Return here if the input is invalid
   	if(playerOneTurn)
   		std::cout << "Player 1 enter your choice(ex: a1): ";
   	else
@@ -33,44 +38,50 @@ int main()
   	std::cin.clear();
   	std::cin.ignore(100, '\n');
 
-
-	x = ((int)choice[0] - 97);
+    //Set the x/y using the ascii character values of the first and second characters in their input
+	  x = ((int)choice[0] - 97);
   	y = ((int)choice[1] - 49);
 
+    //If the space they chose was unoccupied, update the board with their choice
   	if(!board.spaceOccupied(x, y))
   	{
-  		board.updateBoard(x, y, turnP);
-  		if(board.testWin(playerWonP, turnP))
+  		board.updateBoard(x, y, turnP); //Updates the board
+  		if(board.testWin(playerWonP, turnP)) //If they won, then break from the loop and print the final board
       {
         board.printBoard();
   			break;
       }
-
+      //If there wasn't a win, toggle the player's turn. We have to do this after testWin() otherwise we won't know who won
   		if(playerOneTurn)
   			playerOneTurn = false;
   		else
   			playerOneTurn = true;
   	}
-  	else
+  	else //In case they chose a space that already occupied
   	{
   		std::cout << "Space Occupied, please try again." << std::endl;
   		goto GET_INPUT;
   	}
   }
+  //Different scenerios based on which player won, also increments the win variable and tells the player
   if(playerWon)
   {
   	std::cout << "Player 1 won!" << std::endl;
+    xWins++;
+    std::cout << "Player 1 has won " << xWins << " times!" << std::endl;
   }
   else
   {
   	std::cout << "Player 2 won!" << std::endl;
+    oWins++;
+    std::cout << "Player 2 has won " << oWins << " times!" << std::endl;
   }
-
+  //Restarts the game if they want to play again
   std::cout << "Would you like to play again?(y or n): ";
   std::cin >> playAgain;
   if(playAgain == 'y')
   {
-  	board.clear();
+  	board.clear();//Fills the board with 0s
   	cin.clear();
   	cin.ignore(100, '\n');
   	goto GAME_START;
